fc2b6cbf1101d6145a2464e454dae620b890c63e
ma1ucky <79427997+ma1ucky@users.noreply.github.com>
2021-03-20 17:00:35 +0300

    Добавлено обновления долга для изменения электричества

--- view/update_electric.py
+++ view/update_electric.py
@@ -28,5 +28,15 @@ class UpdateElectric(tk.Toplevel):
                                      )
         self.dao.payment.update(payment[1], changes[1], changes[3], payment[4], payment[5], changes[0], payment[7],
                                 self.entry_date_end.get(), changes[2], payment[10], payment[11], self.number)
+        self.debt_update(payment[1], changes[1], payment[4], payment[5], changes[0], changes[2])
         view_payments(self.window, self.tree, self.find_v)
         self.destroy()
+
+    def debt_update(self, lot_number, cost_payment, target_contribution,
+                    membership_fee, electricity, balance,
+                    ):
+        row = list(self.dao.debt.get_by_number(lot_number))
+        debt_id = row[1][0]
+        debt = self.dao.debt.get_by_id(debt_id)
+        self.dao.debt.update(lot_number, debt[2] + float(cost_payment), debt[3] + float(target_contribution),
+                             debt[4] + float(membership_fee), debt[5] + float(electricity), balance, debt_id)
